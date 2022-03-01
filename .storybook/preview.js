c9bef1e4f33379501d2de575ff7ad47a9d8e855c
Frederic Pun <fpun@konradgroup.com>
2022-03-01 03:04:58 -0500

    [UI-4] Init basic storybook setup with scss

---
+++ .storybook/preview.js
@@ -0,0 +1,20 @@
+import { StyleProvider } from '../src/components';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  layout: 'centered',
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+export const decorators = [
+  Story => (
+    <StyleProvider>
+      <Story/>
+    </StyleProvider>
+  ),
+];
