494669f19de485573a01eafd14e9f24bdf7f7614
Joey Siy <61566829+jsiy@users.noreply.github.com>
2023-06-19 16:03:24 -0700

    added customizability to input component (#29)
    * added customizability to input component
    
    * made translucency configurable
    
    * made translucency configurable
    
    * change transluency to opacity to match behaviour, fix undesired falsy, set defaults to maintain old behaviour
    
    ---------
    
    Co-authored-by: David Hui <opensource@davidhui.ca>

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -7,6 +7,16 @@ import styles from './Input.module.scss';
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   /** The default color of the input border */
   outlineColor?: Colors;
+  /** Opacity from 0 to 100, 0 being transparent and 100 being opaque */
+  opacity?: number,
+  /** Opacity from 0 to 100 when the input component is focused, 0 being transparent and 100 being opaque */
+  opacityOnHover?: number,
+  /** Removes border */
+  noBorder?: boolean,
+  /** Changes the color of placeholder text */
+  placeHolderColor?: Colors,
+  /** Changes the color of typed text */
+  textColor?: Colors,
   /** Hides label of input (Only visually) */
   hideLabel?: InputLayoutProps['hideLabel'];
   /** For setting success/error states */
@@ -27,6 +37,11 @@ export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 
 function Input({
   outlineColor = 'shades-0',
+  opacity = 100,
+  opacityOnHover = 100,
+  noBorder = false,
+  placeHolderColor,
+  textColor,
   className,
   hideLabel,
   status,
@@ -40,7 +55,15 @@ function Input({
 }: InputProps) {
   if (status?.state) outlineColor = (status?.state === 'error' ? 'error-500' : 'success');
 
+  const style = {
+    "--input-opacity": (opacity / 100),
+    "--input-opacity-on-hover": (opacityOnHover / 100)
+  } as React.CSSProperties;
+
+  console.log(style);
+
   return (
+    <div style={style}>
     <InputLayout
       required={props.required}
       className={className}
@@ -60,11 +83,16 @@ function Input({
           className={cx(
               outlineColor && styles[`outline--${outlineColor}`],
               styles.input,
+              (opacity !== 100 && opacityOnHover !== 100) && styles[`translucent`],
+              noBorder && styles.noborder,
+              placeHolderColor && styles[`placeholdercolor--${placeHolderColor}`],
+              textColor && styles[`textcolor--${textColor}`],
           )}
           placeholder={label}
           name={name}
           {...props}/>
     </InputLayout>
+    </div>
   );
 }
 
