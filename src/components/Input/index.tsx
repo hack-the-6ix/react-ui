cffbc30d227e9113f5c43d329b5adfc4b2581dbe
Joseph Christian Syjuco Siy <jsiy@uwo.ca>
2023-06-19 00:08:09 -0700

    made translucency configurable

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -7,8 +7,8 @@ import styles from './Input.module.scss';
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   /** The default color of the input border */
   outlineColor?: Colors;
-  /** Sets the background to be translucent (2023 website figma) */
-  translucentBackground?: boolean,
+  /** Translucency from 0 to 100 (2023 website figma), 0 being transparent and 100 being opaque */
+  translucency?: number,
   /** Removes border (2023 website figma) */
   noBorder?: boolean,
   /** Changes the color of placeholder text */
@@ -35,7 +35,7 @@ export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 
 function Input({
   outlineColor = 'shades-0',
-  translucentBackground = false,
+  translucency = 38,
   noBorder = false,
   placeHolderColor,
   textColor,
@@ -52,7 +52,10 @@ function Input({
 }: InputProps) {
   if (status?.state) outlineColor = (status?.state === 'error' ? 'error-500' : 'success');
 
+  const style = { "--translucency": (translucency / 100) } as React.CSSProperties;
+
   return (
+    <div style={style}>
     <InputLayout
       required={props.required}
       className={className}
@@ -72,7 +75,7 @@ function Input({
           className={cx(
               outlineColor && styles[`outline--${outlineColor}`],
               styles.input,
-              translucentBackground && styles.translucent,
+              translucency && styles[`translucent`],
               noBorder && styles.noborder,
               placeHolderColor && styles[`placeholdercolor--${placeHolderColor}`],
               textColor && styles[`textcolor--${textColor}`],
@@ -81,6 +84,7 @@ function Input({
           name={name}
           {...props}/>
     </InputLayout>
+    </div>
   );
 }
 
